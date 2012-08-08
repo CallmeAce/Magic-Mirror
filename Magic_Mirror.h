4fc37b49d6cc9323a8f8edd6a6334d6a6907a637
xuan <xuan@xuan-N53SV.(none)>
2012-08-08 18:27:14 +0200

    some.....

--- Magic_Mirror.h
+++ Magic_Mirror.h
@@ -9,76 +9,46 @@
 #include <iostream>
 #include <boost/thread.hpp>
 #include <boost/date_time.hpp>
-
-
+#include "Magic_Processing.h"
+#include <pcl/common/common_headers.h>
 
 //  Mirror Mirror on the wall..... 
 class Magic_Mirror
 {  
-  private:
-      // parameters setting
-       //   decide the range image getted from point cloud angular resolution 
-      float angular_resolution;
-      const float Roi_Range = 1.5;// default value
-      //    decide the coordinate of range image
-      pcl::RangeImage::CoordinateFrame R_coordinate_frame;
+	private:
 
-      //    parameters for pcl::RangeImage::createFromPointCloud
-      bool setUnseenToMaxRange;
-      float noise_level;
-      float min_range;
-      int border_size;
-      
-       
+    static const int Cluster_NUM = 3;              
 
-  public:
+	public:
+// Variables
+			typedef pcl::PointXYZ PointT;// define the point type
+			pcl::PointCloud<PointT>::Ptr m_cloud_1;// default outcome 1
+			pcl::PointCloud<PointT>::Ptr m_cloud_2;// default outcome 2
+			pcl::PointCloud<PointT>::Ptr m_cloud_3;// default outcome 2
+			std::vector<pcl::PointCloud<PointT>::Ptr,Eigen::aligned_allocator<pcl::PointCloud<PointT>::Ptr > > m_objects_vector;// outcomes from 	
 
-      typedef pcl::PointXYZ PointT;// define the point type
-      pcl::PointCloud<PointT>::Ptr cloud_ptr;
-      pcl::PointCloud<PointT>::Ptr cloud_downS;//outcome of downsampled cloud;
-      pcl::PointCloud<PointT>::Ptr raw_object;
+			Magic_Mirror (); //constractor  //     
+			~Magic_Mirror(); //disconstractor
 
-      Magic_Mirror (); //constractor  //     
-      ~Magic_Mirror(); //disconstractor
-
-      pcl::visualization::PCLVisualizer viewer;
-
-//      pcl::visualization::CloudViewer viewer1;
+			boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+			// pcl::visualization::CloudViewer viewer1;
 
   
 
-// function 
- 
-           // callback to the Opennigrabber, getting data stream from kinect                    
-      void cloud_cb_ (const pcl::PointCloud<PointT>::ConstPtr &cloud);
- 
-           // using octree algorithm compress the data
-      void Octree_Compression (const pcl::PointCloud<PointT>::ConstPtr &cloud);
+// Methods
 
-          // Filter the data and get the data needed
-      void Filters (pcl::PointCloud<PointT>::Ptr & raw_data);
-       
-          // Data Segmentation
-      void Segmentation(pcl::PointCloud<PointT>::Ptr & input_data,pcl::PointCloud<PointT>::Ptr & output_data);
+			// callback to the Opennigrabber, getting data stream from kinect                    
+			void cloud_cb_ (const pcl::PointCloud<PointT>::ConstPtr &cloud);
 
-          // point cloud clustering 
-      void Clustering (pcl::PointCloud<PointT>::Ptr & raw_data,int cluster);   
-
-          // read pcd file from disk
-      void Read_Pcd_f ();
-    
-          // creat range_image from Pointcloud
-      void Creat_Range_Img (const pcl::PointCloud<PointT>::ConstPtr &cloud);
-
-          // extract border from range image
-      void RImg_BorderExtra ();
-
-         // visualization 
-      void Magic_Visualizer ();
+// TODO
+			void Map_updating ();// to be finished (call the mapping class)
+// TODO 
+			void Magic_Visualizer ();// to be finished (call the visualization class)
+            
+            void run (); // The main structure of the vision system
 
 
-
-}
+};
 
 
 
